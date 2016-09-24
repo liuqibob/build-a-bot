414d32c901956cd831d7a85cf03de591e5f2a61b
liuqibob <liuqibob@outlook.com>
2016-09-24 10:53:06 +0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-
 var gameInput = $("#bot-input");
 var gameOutput  = $("#bot-output");
-gameInput.keydown(function(keydownEvent) { 
-// the key code for enter is 13
+gameInput.keydown(function(keydownEvent) {
   if (keydownEvent.keyCode == 13) {      
     parseText(gameInput.val()); 
     gameInput.val("");   
@@ -10,12 +8,13 @@ gameInput.keydown(function(keydownEvent) {
 })
 
 function parseText(text) {      
-if (text === "Good bye") {
-      gameOutput.html("Bye!");
-    }else if (text==="name?") {
-      gameOutput.html("All powerful pizza Bot")
-    } else if (text === "robot come out!") {
-      gameOutput.html("🤖️🤖️")
-    } else if (text==="move"){
-      gameOutput.animate({top:500}); 
+  if (text === "Good bye") {
+    gameOutput.html("Bye!");
+  }else if (text==="name?") {
+    gameOutput.html("All powerful pizza Bot")
+  } else if (text === "robot come out!") {
+    gameOutput.html("🤖️🤖️")
+  } else if (text==="move"){
+    gameOutput.animate({top:500}); 
+  }
 }
